34f2c4bc8ce2f75c00a466a186ae484cb66cd874
shahbandrtenants <tenants@shahbandr.com>
2022-06-18 01:08:26 +0200

    14

--- admin_tune/js/custom-frontend-script.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ajaxComplete(function(event, xhr, settings) {
-	if (settings.url === '/?wc-ajax=update_order_review') {
-		jQuery('body.woocommerce-checkout #neve-checkout-coupon .woocommerce-info .berocket_minmax').parent().hide();
-		jQuery('body.woocommerce-checkout #neve-checkout-coupon .woocommerce-info .have_coupon_span').parent().show();
-	}
-});
