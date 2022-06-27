56684f0c5e77955d1941d04e5f8d4d234a403cec
Erfan Ilyas <repo-bot@waas1.com>
2022-06-27 08:44:38 +0500

    added custom hooks

---
+++ b1-site-create-post-hooks.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package b1-site-create-post-hooks
+ */
+/*
+Plugin Name: b1-site-create-post-hooks.php
+Plugin URI: https://waas1.com/
+Description: custom hooks only for this platform.
+Version: 1.0.0
+Author: Erfan
+Author URI: https://waas1.com/
+License: GPLv2 or later
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+add_action( 'waas1_tenant_post_created', function( $assoc_args ){ //hook as soon as new site is created and ready.
+	
+	//set the default site language to arabic.
+	update_option( 'WPLANG', 'ar' );
+	
+	//rewite permalinks:
+	flush_rewrite_rules();
+	
+	//clear all cache
+	if( function_exists('w3tc_flush_all') ){
+		w3tc_flush_all();
+	}
+	
+	
+});
+
+
+?>
