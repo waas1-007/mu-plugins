a1acd7164b45ec8222e3648dca7fb70f0fc69643
eslam el sherif <dev@eslamelsherif.com>
2022-06-29 01:07:44 +0200

    update

--- a1-uipress-global-settings.php
+++ a1-uipress-global-settings.php
@@ -3,12 +3,12 @@ if (WAAS1_RESTRICTION_GROUP_ID != 1) {
 
     add_action('admin_init', function () {
 
-        remove_submenu_page('options-general.php', 'uip-styles');
-        remove_submenu_page('options-general.php', 'uip-settings');
-        if (isset($_GET['page']) and in_array($_GET['page'], ['uip-styles', 'uip-settings'])) {
-            wp_redirect(admin_url());
-            exit;
-        }
+        // remove_submenu_page('options-general.php', 'uip-styles');
+        // remove_submenu_page('options-general.php', 'uip-settings');
+        // if (isset($_GET['page']) and in_array($_GET['page'], ['uip-styles', 'uip-settings'])) {
+        //     wp_redirect(admin_url());
+        //     exit;
+        // }
     }, 999);
     
 }
