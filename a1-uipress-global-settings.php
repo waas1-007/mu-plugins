fd4b24046aeb64c2315d8cd80dbbfd38ab7cdaaf
eslam el sherif <dev@eslamelsherif.com>
2022-06-28 22:09:35 +0200

    update

--- a1-uipress-global-settings.php
+++ a1-uipress-global-settings.php
@@ -1,7 +1,7 @@
 <?php
 
 foreach (array_diff(scandir(WPMU_PLUGIN_DIR . '/json/uip/'), array('.', '..')) as $key => $__uip) {
-    add_filter('option_' . basename($__uip), function ($plugins) use ($__uip) {
+    add_filter('option_' . basename($__uip,'.json'), function ($plugins) use ($__uip) {
         return json_decode(file_get_contents(WPMU_PLUGIN_DIR . '/json/uip/' . $__uip, true));
     });
 }
