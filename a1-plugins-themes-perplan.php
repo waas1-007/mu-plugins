356fa25a69dd06cf336238d1d5225f9ab84e50d9
eslam el sherif <dev@eslamelsherif.com>
2022-07-01 01:46:33 +0200

    update

--- a1-plugins-themes-perplan.php
+++ a1-plugins-themes-perplan.php
@@ -8,13 +8,19 @@ function _filter_all_plugins($get_plugins)
     $plugins_allowed = [];
 
     $plan = @json_decode(file_get_contents(WPMU_PLUGIN_DIR . '/json/plans/' . WAAS1_RESTRICTION_GROUP_ID . '.json'), true);
+    // $plan = @json_decode(file_get_contents(WPMU_PLUGIN_DIR . '/json/plans/2.json'), true);
     $critical_plugins = @json_decode(file_get_contents(WPMU_PLUGIN_DIR . '/json/plugins/critical_plugins.json'), true);
     $plugins_hidden = @json_decode(file_get_contents(WPMU_PLUGIN_DIR . '/json/plugins/plugins_hidden.json'), true);
 
-    $plugins_hidden = array_merge($critical_plugins, (is_array($plugins_hidden) ? $plugins_hidden :[]));
+    $plugins_hidden = array_merge($critical_plugins, (is_array($plugins_hidden) ? $plugins_hidden : []));
     if (isset($plan['plugins'])) {
         foreach ($get_plugins as $key =>  $plugin) {
+            $plugin['hidden'] = 1;
             if (in_array($key, $plan['plugins']) and !in_array($key, $plugins_hidden)) {
+                $plugins_allowed[$key] = $plugin;
+            } else if ( !in_array($key, $plugins_hidden)){
+                $plugin['hidden'] = 0;
+
                 $plugins_allowed[$key] = $plugin;
             }
         }
@@ -43,6 +49,7 @@ function filter_all_themes($get_themes)
     $themes_allowed = [];
 
     $plan = @json_decode(file_get_contents(WPMU_PLUGIN_DIR . '/json/plans/' . WAAS1_RESTRICTION_GROUP_ID . '.json'), true);
+    // $plan = @json_decode(file_get_contents(WPMU_PLUGIN_DIR . '/json/plans/2.json'), true);
     if (isset($plan['themes'])) {
         foreach ($get_themes as $key =>  $theme) {
             if (in_array($key, $plan['themes'])) {
@@ -55,13 +62,13 @@ function filter_all_themes($get_themes)
 }
 
 if (WAAS1_RESTRICTION_GROUP_ID != 1) {
-    add_filter('all_plugins', '_filter_all_plugins', 99, 1);
+add_filter('all_plugins', '_filter_all_plugins', 99, 1);
 
-    add_filter('wp_prepare_themes_for_js', 'filter_all_themes', 99, 2);
-    add_filter('show_advanced_plugins', function ($default, $type) {
-        if ($type == 'mustuse') return false; // Hide Must-Use
-        return $default;
-    }, 10, 2);
+add_filter('wp_prepare_themes_for_js', 'filter_all_themes', 99, 2);
+add_filter('show_advanced_plugins', function ($default, $type) {
+    if ($type == 'mustuse') return false; // Hide Must-Use
+    return $default;
+}, 10, 2);
 
-    require_once __DIR__ . '/rebranding/rebranding.php';
+require_once __DIR__ . '/rebranding/rebranding.php';
 }
