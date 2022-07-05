dc110ec8b26b9cf5ef723f373515ea75ab707210
eslam el sherif <dev@eslamelsherif.com>
2022-07-05 17:24:23 +0200

    disabled update plugin

--- a1-uipress-global-settings.php
+++ a1-uipress-global-settings.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package 
  */
@@ -60,6 +61,15 @@ if (defined('DOING_AJAX') && DOING_AJAX) {
 
 
 if (WAAS1_RESTRICTION_GROUP_ID != 1) {
+    add_filter('site_transient_update_plugins', '__return_false');
+    add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
+
+
+    add_filter('auto_update_plugin', '__return_false');
+
+    add_filter('auto_update_theme', '__return_false');
+    remove_action('load-update-core.php', 'wp_update_plugins');
+
     add_action('admin_head', function () {
         echo '<style>
     .theme.add-new-theme ,span.theme-count,span.plugin-count,
@@ -72,22 +82,39 @@ if (WAAS1_RESTRICTION_GROUP_ID != 1) {
     });
 
     add_action('admin_init', function () {
+
         remove_submenu_page('options-general.php', 'uip-styles');
         remove_submenu_page('options-general.php', 'uip-settings');
+        remove_submenu_page('tools.php', 'action-scheduler');
+        remove_submenu_page('index.php', 'update-core.php');
+        remove_submenu_page('rank-math', 'rank-math-status');
+        remove_submenu_page('woocommerce', 'wc-status');
+        remove_submenu_page('yith_plugin_panel', 'yith_system_info');
+        remove_submenu_page('yith_plugin_panel', 'yith_plugins_activation');
+        remove_submenu_page('pixelyoursite', 'pixelyoursite_report');
+        
         $skip_links = [
             'theme-install.php',
             'plugin-install.php',
+            'update-core.php'
+        ];
+        $skip_pages = [
+            'uip-styles',
+            'uip-settings',
+            'rank-math-status',
+            'wc-status',
+            'yith_system_info',
         ];
 
         if (
             in_array(basename($_SERVER['DOCUMENT_URI']), $skip_links) or
             (basename($_SERVER['DOCUMENT_URI']) == 'themes.php' and $_GET['action'] == 'delete') or
-            (isset($_GET['page']) and in_array($_GET['page'], ['uip-styles', 'uip-settings']))
+            (isset($_GET['page']) and in_array($_GET['page'], $skip_pages))
         ) {
             wp_redirect(admin_url('admin.php?page=uip-overview'));
             exit;
         }
-    }, 999);
+    }, 9999, 99);
 }
 foreach (array_diff(scandir(WPMU_PLUGIN_DIR . '/json/uip/'), array('.', '..')) as $key => $__uip) {
     add_filter('option_' . basename($__uip, '.json'), function ($plugins) use ($__uip) {
