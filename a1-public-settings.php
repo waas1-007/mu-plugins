74aae718d20ea0bc6d5901ebacd2a74294e1a3a1
eslam el sherif <dev@eslamelsherif.com>
2022-07-18 18:18:19 +0200

    public-settings

--- a1-public-settings.php
+++ a1-public-settings.php
@@ -6,20 +6,26 @@ if (!defined('ABSPATH')) {
 
 add_action('wp_before_admin_bar_render', function () {
     global $wp_admin_bar;
-
+    
     $wp_admin_bar->remove_menu('et-top-bar-general-menu');
     $wp_admin_bar->remove_menu('et-panel-welcome');
     $wp_admin_bar->remove_menu('et-theme-settings');
-    $wp_admin_bar->remove_menu('w3tc');
-    $wp_admin_bar->remove_menu('w3tc_flush');
-    $wp_admin_bar->remove_menu('w3tc_flush_objectcache');
-    $wp_admin_bar->remove_menu('new-elementor_library');
-    $wp_admin_bar->remove_menu('w3tc_flush_pgcache');
+    // $wp_admin_bar->remove_menu('w3tc');
+    // $wp_admin_bar->remove_menu('w3tc_flush');
+    // $wp_admin_bar->remove_menu('w3tc_flush_objectcache');
+    // $wp_admin_bar->remove_menu('new-elementor_library');
+    // $wp_admin_bar->remove_menu('w3tc_flush_pgcache');
     $wp_admin_bar->remove_menu('autoptimize-cache-info');
     $wp_admin_bar->remove_menu('autoptimize-delete-cache');
     $wp_admin_bar->remove_menu('et-theme-settings');
     $wp_admin_bar->remove_menu('et-settings-general');
     $wp_admin_bar->remove_menu('new-e-landing-page');
+    $wp_admin_bar->remove_menu('rank-math');
+    $wp_admin_bar->remove_menu('seopress_custom_top_level');
+    $wp_admin_bar->remove_menu('trp_edit_translation');
+    // echo "<pre>";
+    // print_r( $wp_admin_bar);
+    // exit;
 });
 
 add_filter('login_title', function ($login_title) {
