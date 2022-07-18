8561f29bb03348a28b9b4f77358890be9214e270
eslam el sherif <dev@eslamelsherif.com>
2022-07-19 00:14:18 +0200

    chat

--- a1-public-settings.php
+++ a1-public-settings.php
@@ -6,7 +6,7 @@ if (!defined('ABSPATH')) {
 
 add_action('wp_before_admin_bar_render', function () {
     global $wp_admin_bar;
-    
+
     $wp_admin_bar->remove_menu('et-top-bar-general-menu');
     $wp_admin_bar->remove_menu('et-panel-welcome');
     $wp_admin_bar->remove_menu('et-theme-settings');
@@ -29,7 +29,6 @@ add_action('wp_before_admin_bar_render', function () {
             'href'  => '?remove_cash=remove_cash',
         )
     );
-
 });
 add_action('init', function () {
     if (isset($_GET['remove_cash']) and $_GET['remove_cash']) {
@@ -66,3 +65,70 @@ foreach (array_diff(scandir(WPMU_PLUGIN_DIR . '/json/menu-editor/'), array('.', 
         return json_decode(file_get_contents(WPMU_PLUGIN_DIR . '/json/menu-editor/' . $__editor, true), true);
     });
 }
+
+add_action('admin_footer', function () {
+    if (current_user_can('administrator')) {
+?>
+        <script src="https://snippets.freshchat.com/js/fc-pre-chat-form-v2.min.js"></script>
+        <script>
+            var preChatTemplate = {
+                //Form header color and Submit button color.
+                mainbgColor: '#2d2d2d',
+                //Form Header Text and Submit button text color.
+                maintxColor: '#fff',
+                //Chat Form Title
+                heading: 'خدمة العملاء ',
+                //Chat form Welcome Message
+                textBanner: 'من فضلك ادخل البيانات المطلوبه للتحدث مع خدمة العملاء ',
+                //Submit Button Label.
+                SubmitLabel: 'ابدأ المحادثة',
+                //Fields List - Maximum is 5
+                //All the values are mandatory and the script will not work if not available.
+                fields: {
+                    field2: {
+                        //Type for Name - Do not Change
+                        type: "name",
+                        //Label for Field Name, can be in any language
+                        label: "الاسم",
+                        //Default - Field ID for Name - Do Not Change
+                        fieldId: "name",
+                        //Required "yes" or "no"
+                        required: "yes",
+                        //Error text to be displayed
+                        error: "ادخل الاسم بطريقه صحيحه"
+                    },
+                    field3: {
+                        //Type for Email - Do Not Change
+                        type: "email",
+                        //Label for Field Email, can be in any language
+                        label: " البريد الالكترونى المسجل لدينا",
+                        //Default - Field ID for Email - Do Not Change
+                        fieldId: "email",
+                        //Required "yes" or "no"
+                        required: "yes",
+                        //Error text to be displayed
+                        error: "ادخل عنوان بريد الكترونى صحيح"
+                    },
+                }
+            };
+            window.fcSettings = {
+                token: "b7421e9a-140d-4c46-a3b2-fd9c4ccd1c11",
+                host: "https://wchat.freshchat.com",
+                config: {
+                    cssNames: {
+                        //The below element is mandatory. Please add any custom class or leave the default.
+                        widget: 'custom_fc_frame',
+                        //The below element is mandatory. Please add any custom class or leave the default.
+                        expanded: 'custom_fc_expanded'
+                    }
+                },
+                onInit: function() {
+                    console.log('widget init');
+                    fcPreChatform.fcWidgetInit(preChatTemplate);
+                }
+            };
+        </script>
+        <script src="https://wchat.freshchat.com/js/widget.js" async></script>
+<?php
+    }
+});
